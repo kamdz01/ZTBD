ef245c8e2e59a362a360976a6f43b9f948ad91c6
JBiMa <biegan64maciek@gmail.com>
2025-03-24 18:09:01 +0100

    feat: created runAll script to run all db

---
+++ runAll.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Kopiowanie olist.sqlite do DB/Postgres/import..."
+cp olist.sqlite DB/Postgres/import/olist.sqlite
+
+echo "Uruchamianie CouchDB..."
+(cd DB/CouchDB && docker-compose up -d)
+
+echo "Uruchamianie MongoDB..."
+(cd DB/MongoDB && docker-compose up -d)
+
+echo "Uruchamianie PostgreSQL..."
+(cd DB/Postgres && docker-compose up -d)
+
+echo "Uruchamianie SQLite..."
+(cd DB/SQLite && docker-compose up -d)
+
+if [ ! -d ".venv" ]; then
+    python3 -m venv .venv
+fi
+source .venv/bin/activate
+
+echo "Instalacja zależności backendu..."
+(cd WEB/backend && pip install -r requirements.txt)
+
+echo "Uruchamianie migracji dla CouchDB..."
+(cd DB/CouchDB &&  pip install -r requirements.txt && python3 import_all.py)
+
+echo "Uruchamianie migracji dla MongoDB..."
+(cd DB/MongoDB && pip install -r requirements.txt && python3 migrate.py)
