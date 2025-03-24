a368f56df6a0951ce50b06980643df005b585f51
JBiMa <biegan64maciek@gmail.com>
2025-03-24 19:31:55 +0100

    chore: fixed couchDB

--- DB/MongoDB/migrate.py
+++ DB/MongoDB/migrate.py
@@ -3,20 +3,24 @@ import csv
 from pymongo import MongoClient
 
 # Konfiguracja połączenia – dostosuj DATABASE_URL oraz nazwę bazy danych według potrzeb
-DATABASE_URL = "mongodb://admin:secret@localhost:27017/ecommerce?authSource=admin&directConnection=true"
+DATABASE_URL = "mongodb://admin:admin@localhost:27017/ecommerce?authSource=admin&directConnection=true"
 client = MongoClient(DATABASE_URL)
 db = client["ecommerce"]
 
 # Ścieżka do folderu z plikami CSV
 csv_folder = "../csv_exports"
 
+
 def process_csv_file(file_path, collection_name):
-    with open(file_path, newline='', encoding='utf-8') as csvfile:
+    with open(file_path, newline="", encoding="utf-8") as csvfile:
         reader = csv.DictReader(csvfile)
         data = list(reader)
         if data:
             result = db[collection_name].insert_many(data)
-            print(f"Wstawiono {len(result.inserted_ids)} rekordów do kolekcji {collection_name}.")
+            print(
+                f"Wstawiono {len(result.inserted_ids)} rekordów do kolekcji {collection_name}."
+            )
+
 
 def main():
     # Iteracja po plikach CSV w katalogu csv_exports
@@ -28,5 +32,6 @@ def main():
             print(f"Przetwarzam plik: {filename} do kolekcji: {collection_name}")
             process_csv_file(file_path, collection_name)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
